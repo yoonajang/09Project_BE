a4d503286bf98a5599710b2bf3be6090832d38ec
KYUNGEUNOHH <101075355+KYUNGEUNOHH@users.noreply.github.com>
2022-04-29 01:55:13 +0900

    Revert "Merge remote-tracking branch 'origin' into kyungeun"
    This reverts commit 37c41d03c2d44d4f58a2d1d8bcde6b6bbe7eca27, reversing
    changes made to 7230bfa7f70465aa4aa810b0dcb7d820b2fa4cc7.

--- routes/main.js
+++
@@ -1,8 +0,0 @@
-const express= require('express');
-const router = express.Router();
-const db = require('../config');
-const authMiddleware = require("../middlewares/auth");
-
-
-
-module.exports=router;
