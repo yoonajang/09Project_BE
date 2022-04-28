80f0b52d13dca17e5215293d521724ba4a2f7363
Moonhee <o119a@naver.com>
2022-04-28 13:58:04 +0900

    edit7

--- app.js
+++ app.js
@@ -42,10 +42,7 @@ app.use('/', routers);
 app.get(
  "/.well-known/pki-validation/FEFFF8AAD41B2BDD0AC37B8AE376E000.txt",
  (req, res) => {
-   res.sendFile(__dirname + "/.well-known/pki-validation/FEFFF8AAD41B2BDD0AC37B8AE376E000.txt")
-   || 
-   res.sendFile("www."+__dirname + "/.well-known/pki-validation/FEFFF8AAD41B2BDD0AC37B8AE376E000.txt");
-   console.log(__dirname)
+   res.sendFile("www"+__dirname + "/.well-known/pki-validation/FEFFF8AAD41B2BDD0AC37B8AE376E000.txt")
  }
 );
 
