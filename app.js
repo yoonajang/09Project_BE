41420d11c99a1f1c8554762ad0d1673b6aeba00b
Moonhee <o119a@naver.com>
2022-04-28 14:30:11 +0900

    https!!!

--- app.js
+++ app.js
@@ -10,8 +10,11 @@ const http = require ('http')
 // const AWS = require('aws-sdk');
 const https = require("https");
 const app = express();
-const port = 3000;
- 
+const app_http = express();
+// const port = 3000;
+const httpPort = 80;
+const httpsPort = 443;
+
 
 //소켓
 // const socketIo = require('socket.io');
@@ -39,12 +42,39 @@ app.use(requestMiddleware);
 
 app.use('/', routers);
 
+app_http.use((req, res, next) => {
+  if (req.secure){
+    next();
+  } else {
+    const to = `https://${req.hostname}:${httpsPort}${req.url}`;
+    console.log(to);
+    res.redirect(to);
+  }
+})
+
+
 app.get(
  "/.well-known/pki-validation/FEFFF8AAD41B2BDD0AC37B8AE376E000.txt",
  (req, res) => {
    res.sendFile(__dirname + "/.well-known/pki-validation/FEFFF8AAD41B2BDD0AC37B8AE376E000.txt")
  }
 );
+
+
+const credentials = {
+  key: fs.readFileSync(__dirname + './httpskey/private.key', 'utf8'),
+  cert: fs.readFileSync(__dirname + './httpskey/certificate.crt', 'utf8'),
+  ca: fs.readFileSync(__dirname + './httpskey/ca_bundle.crt', 'utf8'),
+}
+
+
+http.createServer(app_http).listen(httpPort, () => {
+  console.log('http서버가 켜졌어요!')
+})
+
+https.createServer(credentials, app).listen(httpsPort, () => {
+  console.log('https서버가 켜졌어요!')
+})
 
 
 //도메인
