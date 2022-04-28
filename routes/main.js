d3f8dd7520e89c618211d2b4d94d4aaec9c708fc
Moonhee <o119a@naver.com>
2022-04-28 21:12:28 +0900

    get main postlist

--- routes/main.js
+++ routes/main.js
@@ -5,6 +5,20 @@ const db = require('../config');
 // const authMiddleware = require("../middlewares/auth");
 
 
+//테스트용
+router.post('/postList', (req, res) => {
+    const address = req.body.address;
+    const sql = 'INSERT INTO Post(`address`) VALUES (?)'
+
+    db.query(sql, address, (err,data) => {
+        if(err) console.log(err) 
+        res.status(201).send({meg: "success"})
+    })
+
+});
+
+
+
 router.get('/postList', (req, res) => {
     const address = req.body.address;
     const sql = 'select * from Post where address=?'
@@ -13,7 +27,7 @@ router.get('/postList', (req, res) => {
     db.query(sql, address, (err, data) => {
         if (err) console.log(err);
         console.log(data)
-        res.status(201).send({msg:'sucess', });
+        res.status(201).send({msg:'sucess',data});
         
     });
 });
