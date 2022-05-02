5aff61e0f6b2d33f401cbe333657d72d91273b22
KYUNGEUNOHH <101075355+KYUNGEUNOHH@users.noreply.github.com>
2022-05-02 11:36:08 +0900

    prettier

---
+++ s3.js
@@ -0,0 +1,8 @@
+const AWS = require('aws-sdk');
+
+const s3 = new AWS.S3({
+    accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+    region: process.env.AWS_REGION
+});
+module.exports = s3;
