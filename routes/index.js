0526e42d4eee3d9ffa7f8996fb04f34d8b80e30a
mulbinbich <mulbinbich@gmail.com>
2022-05-02 11:46:49 +0900

    update

--- routes/index.js
+++ routes/index.js
@@ -1,17 +1,11 @@
-const express = require("express");
-const userRouter = require("./user");
-const mainRouter = require("./main");
-
-
-
+const express = require('express');
+const userRouter = require('./user');
+const mainRouter = require('./main');
 
 const router = express.Router();
 
 // 앞에 /로 시작됨
 router.use('/user', userRouter);
 router.use('/main', mainRouter);
-
-
-
 
 module.exports = router;
--- routes/main.js
+++ routes/main.js
@@ -1,50 +1,86 @@
-const express= require('express');
+const express = require('express');
 const router = express.Router();
 const db = require('../config');
-const authMiddleware = require("../middlewares/auth");
 
-//호스트 게시글 작성 수정전
-router.post('/hostAdd', authMiddleware, upload.array('postImg', 5), // 이미지 여러개 받기 최대5장
-  async (req, res) => {
-    const today = new Date();
-    const date = today.toLocaleString();
-    const { user } = res.locals.user;
-    console.log(user)
-    const email = res.locals.user.email;
-    const nickName = res.locals.user.nickName;
-    const userProfile = res.locals.user.userProfile;
-    const { postTitle, postDesc, postCharge, address, room, wifi, laundry, parkinglot, latitude, longitude } = req.body;    
-    const postImg = [];
-    console.log(req.body);
-    for (let i = 0; i < req.files.length; i++) {
-      postImg.push(req.files[i]?.location);
-    }
-    console.log(postImg)
-    try {
-      await Posts.create({
-        email,
-        userProfile,
-        date,
-        nickName,
-        postTitle, 
-        postDesc, 
-        postCharge, 
-        address,
-        room, wifi, laundry, parkinglot,
-        latitude: Number(latitude),
-        longitude: Number(longitude),
-        postImg,
-      });
-      res.status(200).send({
-        message: '호스트 게시글이 등록되었습니다.',
-      });
-    } catch (err) {
-      res.status(400).send({
-        message: '호스트 게시글 등록이 실패했습니다.',
-      });
-    }
-  }
+const authMiddleware = require('../middlewares/auth');
+
+const upload = require('../S3/s3');
+const AWS = require('aws-sdk');
+const { is } = require('express/lib/request');
+const s3 = new AWS.S3();
+
+//게시글 작성
+router.post(
+    '/postAdd',
+    authMiddleware,
+    // upload.array('image', 5),
+    upload.single('image'),
+    (req, res, next) => {
+        const title = req.body.title;
+        const content = req.body.content;
+        const price = req.body.price;
+        const headCount = req.body.headCount;
+        const category = req.body.category;
+        const endTime = req.body.endTime;
+        const address = req.body.address;
+        const lat = Number(req.body.lat);
+        const lng = Number(req.body.lng);
+
+        const writer = res.locals.user.userName;
+        const User_userId = res.locals.user.userId;
+
+        // const image = [];
+        // for (let i = 0; i < req.files.length; i++) {
+        //     image.push(req.files[i]?.location);
+        // }
+
+        const image = req.file?.location;
+
+        const isDone = 'false';
+
+        const datas = [
+            title,
+            content,
+            price,
+            headCount,
+            category,
+            endTime,
+            address,
+            lat,
+            lng,
+            writer,
+            User_userId,
+            isDone,
+            image,
+        ];
+        // const datas = {
+        //     title: title,
+        //     content: content,
+        //     price: price,
+        //     headCount: headCount,
+        //     category: category,
+        //     endTime: endTime,
+        //     address: address,
+        //     lat: lat,
+        //     lng: lng,
+        //     writer: writer,
+        //     User_userId: User_userId,
+        //     isDone: isDone,
+        //     image: image
+        // };
+        const sql =
+            'INSERT INTO Post (`title`, `content`, `price`, `headCount`, `category`, `endTime`, `address`, `lat`, `lng`, `writer`, `User_userId`, `isDone`, `image`) VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?)';
+        // const sql = "INSERT INTO Post SET ?";
+
+        db.query(sql, datas, (err, rows) => {
+            if (err) {
+                console.log(err);
+                res.status(401).send({ msg: '글 등록 실패' });
+            } else {
+                res.status(201).send({ msg: '글 등록 성공' });
+            }
+        });
+    },
 );
 
-
-module.exports=router;
+module.exports = router;
