33718b0d991b25b344aa1e7193365891b91e2865
Moonhee <o119a@naver.com>
2022-05-02 17:22:10 +0900

    [docs] 수정

--- app.js
+++ app.js
@@ -68,17 +68,17 @@ const credentials = {
 }
 
 
-// http.createServer(app_http).listen(httpPort, () => {
-//   console.log('http서버가 켜졌어요!')
-// })
+http.createServer(app_http).listen(httpPort, () => {
+  console.log('http서버가 켜졌어요!')
+})
 
-// https.createServer(credentials, app).listen(httpsPort, () => {
-//   console.log('https서버가 켜졌어요!')
-// })
+https.createServer(credentials, app).listen(httpsPort, () => {
+  console.log('https서버가 켜졌어요!')
+})
 
 
 //도메인
-app.listen(port, () => {
-  console.log(port, '포트로 서버가 켜졌어요!')
-}); 
+// app.listen(port, () => {
+//   console.log(port, '포트로 서버가 켜졌어요!')
+// }); 
 
