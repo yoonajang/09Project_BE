06731c0303a39b65d354963037cf09bfcd692686
Moonhee <o119a@naver.com>
2022-05-04 09:51:44 +0900

    [docs] 메인페이지 게시글 수정

--- routes/main.js
+++ routes/main.js
@@ -43,7 +43,7 @@ router.post('/postadd', authMiddleware, upload.single('image'), (req, res, next)
                 console.log(err);
                 res.status(201).send({ msg: 'fail' });
             } else {
-                res.status(201).send({ msg: 'success' });
+                res.status(201).send({ msg: 'success' }, rows);
             }
         });
     },
