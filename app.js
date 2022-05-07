fe2a6d43a3622899dd2e5a4cb82c6911a1cdb8f0
Moonhee <o119a@naver.com>
2022-05-08 06:55:10 +0900

    [docs] 소켓 연결시도3

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ const httpPort = 80;
 const httpsPort = 3443;
 // const httpsPort = 443;
 const path = require('path')
-const {socketIO} = require('socket.io'); //소켓 라이브러리 불러오기
+const socketIO = require('socket.io'); //소켓 라이브러리 불러오기
 const moment = require('moment'); //시간 표시를 위해 사용
 // const connect = require('../config');
 
