72350208fa50541d5c54c50c777d3b4c95d166c9
Moonhee <o119a@naver.com>
2022-05-07 20:27:42 +0900

    [docs] 소켓 Main 수정

--- app.js
+++ app.js
@@ -143,7 +143,9 @@ http.createServer(app_http).listen(httpPort, () => {
   console.log('http서버가 켜졌어요!')
 })
 
-const server = https.createServer(credentials, app).listen(httpsPort, () => {
+const server = https.createServer(credentials, app)
+
+server.listen(httpsPort, () => {
   console.log('https서버가 켜졌어요!')
 })
 
