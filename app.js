0e3160cabb2a8101b269c0564f936009febb1854
Moonhee <o119a@naver.com>
2022-05-08 15:16:08 +0900

    [docs] 소켓 연결시도4

--- app.js
+++ app.js
@@ -83,9 +83,10 @@ server.listen(httpsPort, () => {
     console.log('https서버가 켜졌어요!')
   })
 
+  
 io.on('connection', socket => {
     console.log('연결성공');
-    메세지 주고 받기
+    // 메세지 주고 받기
     socket.on('sendmessage', param => {
         //프론트 입력값 받아주는 코드
         //chat table data 입력
