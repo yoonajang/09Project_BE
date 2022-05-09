0b002b8d68a09c52226e55db1157d60fdc1ef24b
Moonhee <o119a@naver.com>
2022-05-09 18:16:57 +0900

    [docs] 마이리스트 수정완료

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const userRouter = require('./user');
+const meRouter = require('./me');
 const mainRouter = require('./main');
 const chatRouter = require('./chat');
 
@@ -7,6 +8,7 @@ const router = express.Router();
 
 // 앞에 /로 시작됨
 router.use('/user', userRouter);
+router.use('/user', meRouter);
 router.use('/main', mainRouter);
 router.use('/main', chatRouter);
 
