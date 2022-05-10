8fb300cdbe0a9455f674ca303abb163974bf64ef
Moonhee <o119a@naver.com>
2022-05-10 13:08:56 +0900

    CD 적용

--- app.spec.js
+++ app.spec.js
@@ -16,9 +16,8 @@ describe('연결 테스트', () => {
     test("테스트 설명이 들어갑니다. 숫자1은 1의 결과를 줍니다.", () => {
       expect(1).toBe(1);
     });
- 
 
-    
+
     // test('/user/11 경로에 요청했을 때 status code가 200이어야 한다.', async () => {
     //   const res = await supertest(app).get("/index.html");
     //   expect(res.status).toEqual(200);
