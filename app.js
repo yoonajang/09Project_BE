f130c2729b71984426ceaf83ed3ac5355c2aca78
Moonhee <o119a@naver.com>
2022-05-11 17:10:34 +0900

    [docs] 소켓 알람3

--- app.js
+++ app.js
@@ -136,6 +136,7 @@ io.on('connection', socket => {
     
      //알림기능
      socket.on('pushalarm', param => {
+        console.log(param)
         const postid = param.postid;
         const postId = postid.replace('p', '');
         const userName = param.selectedUser.User_userName;
