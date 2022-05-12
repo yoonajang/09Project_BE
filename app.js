70856503bc0c73ce766a811e8e1a90f3227143b0
Moonhee <o119a@naver.com>
2022-05-12 09:32:52 +0900

    [docs] 소켓 disconnet2

--- app.js
+++ app.js
@@ -361,7 +361,7 @@ io.on('connection', socket => {
     //     io.to(postId).emit('onDisconnect', userName + ' 님이 퇴장했습니다.');
     // })
 
-    socket.on('disconnect', param => {
+    socket.on('leave chatroom', param => {
         console.log(param, '방 나감')
         const postId = param.postid;
         console.log(socket.rooms, '방 나감22222222')
