93fd6e9f657aa1e06f9b4db242dca05adb59bdab
Moonhee <o119a@naver.com>
2022-05-17 15:04:21 +0900

    [docs] JoinPost 수정60

--- app.js
+++ app.js
@@ -353,6 +353,7 @@ io.on('connection', socket => {
     socket.on('leave chatroom', (postid, user) => {
         //param 콘솔로 찍어보기....
         console.log(1)
+        console.log(postid, user)
      
         const postId = postid.replace('p', '');
         const userId = user;
