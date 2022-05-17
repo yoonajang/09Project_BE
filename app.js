f8dc502010a453bf13f47bd42dfcc17f51401f0e
Moonhee <o119a@naver.com>
2022-05-18 08:11:06 +0900

    [style] socket 분리

--- app.js
+++ app.js
@@ -13,12 +13,12 @@ const port = 3000;
 const httpPort = 80;
 const httpsPort = 443;
 const path = require('path');
-const { Server } = require('socket.io'); //소켓 라이브러리 불러오기
+// const { Server } = require('socket.io'); //소켓 라이브러리 불러오기
 const SocketIO = require("./socket");
-const mysql = require('mysql');
-const moment = require('moment'); //시간 표시를 위해 사용
-const db = require('./config');
-const { clearCache } = require('ejs');
+// const mysql = require('mysql');
+// const moment = require('moment'); //시간 표시를 위해 사용
+// const db = require('./config');
+// const { clearCache } = require('ejs');
 
 app.use(cors());
 
@@ -28,7 +28,7 @@ const credentials = {
     ca: fs.readFileSync(__dirname + '/ca_bundle.crt', 'utf8'),
 };
 
-const server = https.createServer(credentials, app);
+// const server = https.createServer(credentials, app);
 // --------------------------------------
 // const io = new Server(server, {
 //     cors: {
