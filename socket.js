91eee005cae3092435b559bd9375a9473b8457c8
Moonhee <o119a@naver.com>
2022-05-19 12:24:04 +0900

    [docs] edit socket1

--- socket.js
+++ socket.js
@@ -67,6 +67,12 @@ module.exports = (server) => {
                             console.log(userId,'다있는데, 너는 참가자 아니야. fail')
                             const status = "fail"
                             socket.to(userId).emit('block chatroom', status) 
+
+                            io.to(postid).emit(
+                                'connected',
+                                userName + ' 님이 입장했습니다.',
+                            );
+
                         }
                     } else if (foundJoin[0].headCount > foundJoin[0].count) {
                         console.log(userId,'sucess','아직널널해')
