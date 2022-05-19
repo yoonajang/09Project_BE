fed4568921f7468945fcc82d3297999eae7e0c1a
Moonhee <o119a@naver.com>
2022-05-19 20:18:39 +0900

    git ignore add

---
+++ kakao-auth/index.js
@@ -0,0 +1,61 @@
+require('dotenv').config();
+const passport = require('passport');
+const db = require('../config');
+// const { connect } = require('../config');
+const KakaoStrategy = require('passport-kakao').Strategy;
+// const User = require('../schemas/user.schemas');
+
+console.log(process.env.KAKAOCLIENT_ID, '>>>>>')
+
+module.exports = () => {
+    passport.use(
+        new KakaoStrategy(
+            {
+                clientID: process.env.KAKAOCLIENT_ID,
+                clientSecret: process.env.KAKAOSECRET,
+                callbackURL: process.env.KAKAOCALLBACKURL,
+            },
+
+            async (accessToken, refreshToken, profile, done) => {
+                console.log('try in', profile);
+                const sql = 'select * from User where userEmail = ?';
+                const post = [NewUserEmail];
+
+                db.query(sql, post, (err, results, fields) => {
+                    if (err) {
+                        console.log(err);
+                        done(err);
+                    }
+                    if (results.length === 0) {
+                        // 해당 유저가 존재하지 않는다면, 새로운 아이디를 만들어주고 로그인 시켜줌.
+                        const sql =
+                            'INSERT User(userEmail, password) values(?,?)';
+                        const post = [NewUserEmail, NewUserPassword];
+                        db.query(sql, post, (err, results, fields) => {
+                            if (err) {
+                                console.log(err);
+                                done(err);
+                            }
+                            const sql =
+                                ' SELEVT * FROM User Where userEmail =?'; //가입이 완료되었으면 로그인
+                            const post = [NewUserEmail];
+                            db.query(sql, post, (err, results, fields) => {
+                                if (err) {
+                                    console.log(err);
+                                    done(err);
+                                }
+                                const user = results[0];
+                                return done(null, user);
+                            });
+                        });
+                    } else {
+                        //이미 유저가 존재한다면 바로 로그인
+                        const user = results[0];
+                        return done(null, user);
+                    }
+                });
+            },
+        ),
+    );
+};
+
---
+++ kakao-auth/kakao/kakao.js
@@ -0,0 +1,35 @@
+require('dotenv').config();
+const express = require('express');
+const router = express.Router();
+const passport = require('passport');
+const jwt = require('jsonwebtoken');
+// const upload = require('../../routes/multer/uploads');
+
+router.get(
+    '/kakao',
+    // upload.single('profileImage'),
+    passport.authenticate('kakao'),
+);
+
+const kakaoCallback = (req, res, next) => {
+    passport.authenticate('kakao', { failureRedirect: '/' }, (err, user) => {
+        if (err) return next(err);
+        const { userId, provider, introduce, profileImage, nickname, type } =
+            user;
+        const token = jwt.sign({ userId: userId }, process.env.JWT_SECRET);
+
+        result = {
+            token,
+            // userId,
+            // provider,
+            // profileImage,
+            // nickname,
+            // introduce,
+            // type,
+        };
+        console.log('result', result);
+        res.send({ user: result });
+    })(req, res, next);
+};
+router.get('kakao-auth/kakao/callback', kakaoCallback);
+module.exports = router;
