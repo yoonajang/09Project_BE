2d0c55237ca8f6a3d78a499ab1ecdfc561cc7b48
Moonhee <o119a@naver.com>
2022-05-21 17:24:18 +0900

    [docs] edit-socket-sendMsg18

--- socket.js
+++ socket.js
@@ -235,7 +235,7 @@ module.exports = (server) => {
                                     } else if(user.isLogin === 1 && user.isConnected === 0){
                                         // 알림찾기
                                         db.query('SELECT status, User_userId FROM Alarm WHERE status=? AND User_userId=?', [status,joinUserId], (err, foundUser) => {
-                                            
+                                            console.log(status, joinUserId)
                                             // 알림 있으면 count = +1
                                             console.log(foundUser,2)
                                             // console.log(foundUser[0].status,status, foundUser[0].status===status)
