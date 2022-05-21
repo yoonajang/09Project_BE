a199baf277f2381f4ea8140dbfdbb4b96243a322
Moonhee <o119a@naver.com>
2022-05-22 00:27:13 +0900

    [docs] edit socket_getchat7

--- socket.js
+++ socket.js
@@ -109,21 +109,22 @@ module.exports = (server) => {
                         } else {
                             console.log(userId, 'fail!!!!!!!!!!!!!!!!!!!!!!!')
                             const status = 'fail';
+                            io.emit('block', 'fail');
 
-                            db.query('SELECT * FROM `JoinPost` JP WHERE JP.Post_postId = ? AND JP.isPick = 0;', postId,(err, noPick) => {
+                            // db.query('SELECT * FROM `JoinPost` JP WHERE JP.Post_postId = ? AND JP.isPick = 0;', postId,(err, noPick) => {
 
-                                db.query(
-                                    'SELECT JP.User_userId, JP.User_userEmail, JP.User_userName, JP.userImage, JP.Post_postId FROM `JoinPost` JP LEFT OUTER JOIN `Post` P ON JP.Post_postId = P.postId WHERE JP.isPick=1 AND JP.Post_postId =? AND JP.User_userId NOT IN (P.User_userId) GROUP BY JP.User_userId, JP.User_userEmail, JP.User_userName, JP.userImage, JP.Post_postId;', postId, (err, Pick) => {
+                            //     db.query(
+                            //         'SELECT JP.User_userId, JP.User_userEmail, JP.User_userName, JP.userImage, JP.Post_postId FROM `JoinPost` JP LEFT OUTER JOIN `Post` P ON JP.Post_postId = P.postId WHERE JP.isPick=1 AND JP.Post_postId =? AND JP.User_userId NOT IN (P.User_userId) GROUP BY JP.User_userId, JP.User_userEmail, JP.User_userName, JP.userImage, JP.Post_postId;', postId, (err, Pick) => {
 
-                                        db.query('SELECT User_userId FROM Post WHERE postId = ?', postId, (err, bossId) => {
-                                            const userLists  = [param.loggedUser, noPick, Pick, bossId]
+                            //             db.query('SELECT User_userId FROM Post WHERE postId = ?', postId, (err, bossId) => {
+                            //                 const userLists  = [param.loggedUser, noPick, Pick, bossId]
 
-                                            io.emit('block', 'fail');
+                            //                 io.emit('block', 'fail');
 
-                                        })
-                                })
+                            //             })
+                            //     })
                                  
-                            })
+                            // })
                         }  
                     } else if (foundJoin[0].headCount > foundJoin[0].count) {
                         console.log(33333333333333333333333333)
