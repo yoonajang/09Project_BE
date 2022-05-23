40b6664128f151dfd21a95b2c5ca8e67d73a34e3
Moonhee <o119a@naver.com>
2022-05-23 20:23:34 +0900

    [docs] svg image resize4

--- S3/s3.js
+++ S3/s3.js
@@ -26,7 +26,8 @@ const upload = multer({
                 cb(null, Date.now().toString() + '_origin' + extension);
               },
               transform: function (req, file, cb) {
-                cb(null, sharp().jpeg());
+                // cb(null, sharp().jpeg());
+                cb(null, sharp().png().toFile());
               }, 
             },
             {
