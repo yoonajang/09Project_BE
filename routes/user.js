561b22239a27ea8e2fda371f9aea503d5c4ffcd2
Moonhee <o119a@naver.com>
2022-05-23 17:15:05 +0900

    [docs] islogin alarm

--- routes/user.js
+++ routes/user.js
@@ -187,31 +187,30 @@ router.post('/login', (req, res) => {
 
                     const userId = data[0].userId
 
-
                     // 알림
                     // SendMessage (게시물당 1개씩 알림보내기)
                     const sql_1 = 
-                        'SELECT A.alarmId, A.status, A.userImage, A.createdAt, A.Post_postId, A.type FROM Alarm A WHERE A.User_userId=? AND A.type="sendMessage" AND A.isChecked = 0 GROUP BY A.type, A.Post_postId;';
+                        'SELECT A.alarmId, A.status, A.createdAt, A.Post_postId, A.type, P.title, P.reImage FROM Alarm A Join Post P ON P.postId = A.Post_postId WHERE A.User_userId=? AND A.type="sendMessage" AND A.isChecked = 0 GROUP BY A.type, A.Post_postId;';
                     const sql_1s = mysql.format(sql_1, userId);
 
                     // leaveChat (모든 알림 다보내기)
                     const sql_2 = 
-                        'SELECT alarmId, status, userImage, createdAt, Post_postId, type FROM Alarm WHERE User_userId=? AND type="leaveChat" AND isChecked = 0 ;';
+                        'SELECT A.alarmId, A.status, A.createdAt, A.Post_postId, A.type, P.title, P.reImage FROM Alarm A Join Post P ON P.postId = A.Post_postId WHERE A.User_userId=? AND type="leaveChat" AND isChecked = 0 ;';
                     const sql_2s = mysql.format(sql_2, userId);
 
                     // blockChat (모든 알림 다보내기)
                     const sql_3 = 
-                        'SELECT alarmId, status, userImage, createdAt, Post_postId, type FROM Alarm WHERE User_userId=? AND type="blockChat" AND isChecked = 0 ;';
+                        'SELECT A.alarmId, A.status, A.createdAt, A.Post_postId, A.type, P.title, P.reImage FROM Alarm A Join Post P ON P.postId = A.Post_postId WHERE A.User_userId=? AND type="blockChat" AND isChecked = 0 ;';
                     const sql_3s = mysql.format(sql_3, userId);
 
                     // addDeal (모든 알림 다보내기)
                     const sql_4 = 
-                        'SELECT alarmId, status, userImage, createdAt, Post_postId, type FROM Alarm WHERE User_userId=? AND type="addDeal" AND isChecked = 0 ;';
+                        'SELECT A.alarmId, A.status, A.createdAt, A.Post_postId, A.type, P.title, P.reImage FROM Alarm A Join Post P ON P.postId = A.Post_postId WHERE A.User_userId=? AND type="addDeal" AND isChecked = 0 ;';
                     const sql_4s = mysql.format(sql_4, userId);
 
                     // byebye (모든 알림 다보내기)
                     const sql_5 = 
-                        'SELECT alarmId, status, userImage, createdAt, Post_postId, type FROM Alarm WHERE User_userId=? AND type="byebye" AND isChecked = 0 ;';
+                        'SELECT A.alarmId, A.status, A.createdAt, A.Post_postId, A.type, P.title, P.reImage FROM Alarm A Join Post P ON P.postId = A.Post_postId WHERE A.User_userId=? AND type="byebye" AND isChecked = 0 ;';
                     const sql_5s = mysql.format(sql_5, userId);
 
                     db.query(sql_1s + sql_2s + sql_3s + sql_4s + sql_5s, (err, rows) => {
@@ -268,29 +267,28 @@ router.get('/islogin', authMiddleware, (req, res) => {
 
     // SendMessage (게시물당 1개씩 알림보내기)
     const sql_1 = 
-        'SELECT A.alarmId, A.status, A.userImage, A.createdAt, A.Post_postId, A.type FROM Alarm A WHERE A.User_userId=? AND A.type="sendMessage" AND A.isChecked = 0 GROUP BY A.type, A.Post_postId;';
+        'SELECT A.alarmId, A.status, A.createdAt, A.Post_postId, A.type, P.title, P.reImage FROM Alarm A Join Post P ON P.postId = A.Post_postId WHERE A.User_userId=? AND A.type="sendMessage" AND A.isChecked = 0 GROUP BY A.type, A.Post_postId;';
     const sql_1s = mysql.format(sql_1, userId);
 
     // leaveChat (모든 알림 다보내기)
     const sql_2 = 
-        'SELECT alarmId, status, userImage, createdAt, Post_postId, type FROM Alarm WHERE User_userId=? AND type="leaveChat" AND isChecked = 0 ;';
+        'SELECT A.alarmId, A.status, A.createdAt, A.Post_postId, A.type, P.title, P.reImage FROM Alarm A Join Post P ON P.postId = A.Post_postId WHERE A.User_userId=? AND type="leaveChat" AND isChecked = 0 ;';
     const sql_2s = mysql.format(sql_2, userId);
 
     // blockChat (모든 알림 다보내기)
     const sql_3 = 
-        'SELECT alarmId, status, userImage, createdAt, Post_postId, type FROM Alarm WHERE User_userId=? AND type="blockChat" AND isChecked = 0 ;';
+        'SELECT A.alarmId, A.status, A.createdAt, A.Post_postId, A.type, P.title, P.reImage FROM Alarm A Join Post P ON P.postId = A.Post_postId WHERE A.User_userId=? AND type="blockChat" AND isChecked = 0 ;';
     const sql_3s = mysql.format(sql_3, userId);
 
     // addDeal (모든 알림 다보내기)
     const sql_4 = 
-        'SELECT alarmId, status, userImage, createdAt, Post_postId, type FROM Alarm WHERE User_userId=? AND type="addDeal" AND isChecked = 0 ;';
+        'SELECT A.alarmId, A.status, A.createdAt, A.Post_postId, A.type, P.title, P.reImage FROM Alarm A Join Post P ON P.postId = A.Post_postId WHERE A.User_userId=? AND type="addDeal" AND isChecked = 0 ;';
     const sql_4s = mysql.format(sql_4, userId);
 
     // byebye (모든 알림 다보내기)
     const sql_5 = 
-        'SELECT alarmId, status, userImage, createdAt, Post_postId, type FROM Alarm WHERE User_userId=? AND type="byebye" AND isChecked = 0 ;';
+        'SELECT A.alarmId, A.status, A.createdAt, A.Post_postId, A.type, P.title, P.reImage FROM Alarm A Join Post P ON P.postId = A.Post_postId WHERE A.User_userId=? AND type="byebye" AND isChecked = 0 ;';
     const sql_5s = mysql.format(sql_5, userId);
-
     
     db.query(sql_1s + sql_2s + sql_3s + sql_4s + sql_5s, (err, rows) => {
         if (err) {
