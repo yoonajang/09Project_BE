1dd54e29528b018b6643fe1e327ed099e143e32e
Moonhee <o119a@naver.com>
2022-05-25 00:25:40 +0900

    [docs] add party7_console17

--- routes/post.js
+++ routes/post.js
@@ -20,8 +20,6 @@ router.get('/:postId', (req, res) => {
     db.query(sql, postId, (err, data) => {
         if (err) console.log(err);
 
-        console.log(data)
-
         let head = data[0].headList;
         const bossId = data[0].User_userId
         let newList =[];
