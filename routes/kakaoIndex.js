3e3cb3c36d481858da637746496807aa673a5181
Moonhee <o119a@naver.com>
2022-05-25 15:18:30 +0900

    [docs] register userImage_kakao

--- routes/kakaoIndex.js
+++ routes/kakaoIndex.js
@@ -29,11 +29,14 @@ module.exports = () => {
                 let userImage = profile._json.properties.profile_image
 
                 const defaultKakaoOriginImage = 'http://k.kakaocdn.net/dn/dpk9l1/btqmGhA2lKL/Oz0wDuJn1YV2DIn92f6DVK/img_640x640.jpg'
-
-                // 엔빵 프로필이미지
-                const defaultImage = 'https://nbbang-resizing.s3.ap-northeast-2.amazonaws.com/w_200/1653383370230_resized.png'
-                const defaultOriginImage = 'https://nbbang-resizing.s3.ap-northeast-2.amazonaws.com/w_200/1653383370230_origin.png'
                 
+                // 랜덤 프로필 이미지
+                const Index =  Math.floor(Math.random()*4)
+                const profileImages = ['1653383370230','1653383345720','1653383406785','1653381889650']
+                const baseURL = 'https://nbbang-resizing.s3.ap-northeast-2.amazonaws.com/w_200/'
+
+                const defaultImage = baseURL + profileImages[Index] +'_resized.png'
+                const defaultOriginImage = baseURL + profileImages[Index] +'_origin.png'
                 
                 if (userImage === defaultKakaoOriginImage){
                     reUserImage = defaultImage
@@ -57,7 +60,7 @@ module.exports = () => {
                         // 해당 유저가 존재하지 않는다면, 새로운 아이디를 만들어주고 로그인 시켜줌.
 
                         const sql =
-                            'INSERT User(userEmail, userName, provider, kakaoId, point, userImage,reUserImage) values(?,?,?,?,?,?,?)';
+                            'INSERT User(userEmail, userName, provider, kakaoId, point, userImage, reUserImage) values(?,?,?,?,?,?,?)';
 
                         db.query(sql, params, (err, results) => {
                             if (err) {
